7d70fb304225213bc44f411ca7419698a6dd4851
Amber Meritt <36485750+XanaNite@users.noreply.github.com>
2025-12-14 17:31:14 -0700

    Add [ValidateNever] and ImageFile to Product and Ingredient
    Applied [ValidateNever] to navigation properties in Product and Ingredient models to skip validation. Added a non-mapped ImageFile property to Product for file uploads. Also updated using directives as needed.

--- MerittRestuarant/Models/Ingredient.cs
+++ MerittRestuarant/Models/Ingredient.cs
@@ -1,10 +1,14 @@
-﻿namespace MerittRestuarant.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace MerittRestuarant.Models
 {
     public class Ingredient
     {
         public int IngredientId { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
+
+        [ValidateNever]
         public ICollection<ProductIngredient>? ProductIngredients { get; set; }
     }
 }
--- MerittRestuarant/Models/Product.cs
+++ MerittRestuarant/Models/Product.cs
@@ -1,4 +1,7 @@
-﻿namespace MerittRestuarant.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MerittRestuarant.Models
 {
     public class Product
     {
@@ -7,11 +10,19 @@
         public string? Description { get; set; }
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
-        public string ImageUrl { get; set; } = "https://picsum.photos/id/292/200/300";
         public int CategoryId { get; set; }
+        public string ImageUrl { get; set; } = "https://picsum.photos/id/292/200/300";
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+
+        [ValidateNever]
         public Category Category { get; set; }
+
+        [ValidateNever]
         public ICollection<OrderItem> OrderItems { get; set; }
+
+        [ValidateNever]
         public ICollection<ProductIngredient>  ProductIngredients { get; set; }
-        
     }
 }
